edfcad4937ea46b47309616d90eeb0b396d7e228
j050622 <xiaoping010@163.com>
2018-07-28 16:37:05 +0800

    Update run-vsftpd.sh

--- run-vsftpd.sh
+++ run-vsftpd.sh
@@ -32,6 +32,12 @@ fi
 echo "pasv_address=${PASV_ADDRESS}" >> /etc/vsftpd/vsftpd.conf
 echo "pasv_max_port=${PASV_MAX_PORT}" >> /etc/vsftpd/vsftpd.conf
 echo "pasv_min_port=${PASV_MIN_PORT}" >> /etc/vsftpd/vsftpd.conf
+if [ "${VSFTPD_CONF}" = "none" ]
+then
+        echo ""
+else
+        echo -e "${VSFTPD_CONF}" >> /etc/vsftpd/vsftpd.conf
+fi
 # Get log file path
 export LOG_FILE=`grep xferlog_file /etc/vsftpd/vsftpd.conf|cut -d= -f2`
 
